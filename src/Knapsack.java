3a5c3dd83f5c7bb16c1504c982cc6a311167ed16
McBadass <tvories@gmail.com>
2014-03-16 08:22:54 -0600

    Initial commit

---
+++ src/Knapsack.java
@@ -0,0 +1,21 @@
+//  Program Name  		:	Knapsack.java
+//  Description   		:   Description
+//						:
+//						:
+//                      :
+//  Date               	:	3/10/14
+//  Software Developer 	:	Taylor Vories CS 2050
+//  Remarks            	:
+//
+//
+
+public class Knapsack
+{
+	private int knapSize;
+	private KnapsackItem[] knapArr;
+
+	public Knapsack(int size)
+	{
+		this.knapSize = size;
+	}
+}
---
+++ src/KnapsackItem.java
@@ -0,0 +1,22 @@
+//  Program Name  		:	KnapsackItem.java
+//  Description   		:   Description
+//						:
+//						:
+//                      :
+//  Date               	:	3/10/14
+//  Software Developer 	:	Taylor Vories CS 2050
+//  Remarks            	:
+//
+//
+
+public class KnapsackItem
+{
+	private int size;
+	private int value;
+
+	public KnapsackItem(int size, int value)
+	{
+		this.size = size;
+		this.value = value;
+	}
+}
